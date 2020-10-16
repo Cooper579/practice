ee6a81fe73006ddd27c604dbfa4e46803c79ffc6
Cooper579 <552687@my.cuhsd.org>
2020-10-16 18:49:27 +0000

    makes the stars.

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
+
 import turtle as trtl
 
-#defines house
-#takes two parameters
-#size of the house and pensize
-def house(size,pen):
-  #turns the pen color to blue and the pen size to what was set
-  trtl.color("royalblue")
-  trtl.pensize(pen)
-  trtl.setheading(-90)
-  trtl.pendown()
-  #makes the bottom of the house
-  for i in range(3):
-    trtl.forward(size*1.25)
-    trtl.left(90)
-  #makes the triangles
-  trtl.color("steelblue")
-  trtl.penup()
-  trtl.right(90)
-  trtl.forward(size*2)
-  trtl.left(90)
-  trtl.forward((size*1.25)/2)
-  trtl.pendown()
-  trtl.circle(size,360,3)
-  #makes a door
-  trtl.color("black")
-  trtl.pensize(pen/1.5)
-  trtl.penup()
-  trtl.left(90)
-  trtl.forward(size*3.5)
-  trtl.left(180)
-  trtl.pendown()
-  trtl.forward(size/1.5)
+# The two "def" statements below define the code for create large and small stars
+def star(size):
+    circle_size = size
+    for i in range(size):
+        trtl.circle(circle_size,90)
+        circle_size = circle_size - 1
+    
 
-
-for i in range(3):
-  house(10,15)
-  trtl.penup()
-  trtl.setheading(0)
-  trtl.forward(20)
-  trtl.setheading(90)
-  trtl.forward(10)
-  trtl.setheading(0)
+#This code draws the stars in different sizes at different positions on the drawing
+trtl.speed(0)
+trtl.color("yellow")
+trtl.penup()
+trtl.setposition(-190,75)
+trtl.pendown()
+star(50)
+trtl.penup()
+trtl.setposition(15,190)
+trtl.pendown()
+star(45)
+trtl.penup()
+trtl.setposition(75,90)
+trtl.pendown()
+star(30)
+trtl.penup()
+trtl.setposition(-100,195)
+trtl.pendown()
+star(25)
+trtl.penup()
+trtl.setposition(-60,85)
+trtl.pendown()
+star(20)
 
 wn = trtl.Screen()
 wn.mainloop()
